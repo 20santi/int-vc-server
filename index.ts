78f588049454c5ea0f3f64a07cdcff457a2ce408
syndg <sinchandsgpt9@gmail.com>
2024-02-29 23:20:17 +0530

    Init

---
+++ index.ts
@@ -0,0 +1,35 @@
+import { Socket } from "socket.io";
+
+const http = require("http");
+const express = require("express");
+const { Server } = require("socket.io");
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket: Socket) => {
+  console.log("Connected: ", socket.id);
+
+  socket.on("on-client-connect", () => {
+    socket.broadcast.emit("get-editor-state");
+  });
+
+  socket.on("editor-state", (code: string) => {
+    socket.broadcast.emit("editor-state-from-server", code);
+  });
+
+  socket.on("code-change", (code: string) => {
+    socket.broadcast.emit("code-change", code);
+  });
+});
+
+server.listen(8000, () => {
+  console.log("Server listening on PORT: 8000");
+});
